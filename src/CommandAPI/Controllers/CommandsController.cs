7a674c4b39d3c4689020d99b9fa310494148aa9d
AShaver43 <gofrodude_197@hotmail.com>
2020-09-13 02:25:31 -0700

    More Tests

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -18,5 +18,19 @@ namespace CommandAPI.Controllers
         {
             return _context.CommandItems;
         }
+
+        //GET: api/commands/{Id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+
+            if (commandItem == null)
+                return NotFound();
+
+
+            return commandItem;
+        }
+
     }
 }
